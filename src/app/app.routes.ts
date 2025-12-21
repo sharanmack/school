0febe66417eb36825c9eb19d33da66ec280a481d
mythi-li <mythum495@gmail.com>
2025-12-21 07:45:45 +0530

    21/12/2025 / day one

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  // Later you can add:
-  // { path: 'about', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
   // { path: 'admissions', component: AdmissionsComponent },
   // { path: 'contact', component: ContactComponent }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
